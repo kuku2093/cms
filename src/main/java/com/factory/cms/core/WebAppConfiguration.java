b670ecad8a9b0eb4298c3f9bf68673ee44a220ec
chenjian <chenjian@chenjian-PC>
2018-04-18 16:54:12 +0800

    更换密码操作

--- src/main/java/com/factory/cms/core/WebAppConfiguration.java
+++ src/main/java/com/factory/cms/core/WebAppConfiguration.java
@@ -1,5 +1,9 @@
 package com.factory.cms.core;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 /**
 	* @Description: 对文件的路径进行配置,创建一个虚拟路径/Path/** ，即只要在<img src="/path/picName.jpg" />便可以直接引用图片
 	*这是图片的物理路径  "file:/+本地图片的地址"
@@ -13,4 +17,5 @@ public class WebAppConfiguration extends WebMvcConfigurerAdapter {
 		        super.addResourceHandlers(registry);
 	 }
 
-}*/
+}
+*/
